1ec417ea06b915283149e83b12a244b5d2412ddc
Thomas Garayua <tgarayua@gmail.com>
2020-09-23 12:08:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,11 +2,14 @@ require 'pry'
 
 def find_even_values(src)
   idx = 0 
-  while idx < src.length 
-  do src[idx] % 2 == 0  # conditional using modulo operator
-    puts src[idx]       # puts out even number
-  end
-  idx += 1
+  
+  while idx < src.count do 
+    element_index = 0 
+    while element_index < src[idx].count do
+      p src[idx][element_index]
+      element_index += 1 
+    end
+    idx += 1
 end
 
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
